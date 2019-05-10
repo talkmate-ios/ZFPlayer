ee571064ea3e4ae93c4a2e75c600b668471ca57a
Andy <andy_zhao@talkmate.com>
2019-05-10 17:51:32 +0800

    设置图片模式

--- ZFPlayer/Classes/ControlView/ZFPlayerControlView.m
+++ ZFPlayer/Classes/ControlView/ZFPlayerControlView.m
@@ -758,7 +758,7 @@
     if (!_coverImageView) {
         _coverImageView = [[UIImageView alloc] init];
         _coverImageView.userInteractionEnabled = YES;
-        _coverImageView.contentMode = UIViewContentModeScaleAspectFit;
+        _coverImageView.contentMode = UIViewContentModeScaleAspectFill;
     }
     return _coverImageView;
 }
